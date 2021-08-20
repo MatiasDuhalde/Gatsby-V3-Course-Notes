48f881dda806858c87e56a8102458a347c384e21
Matias Duhalde <matias.duhalde@uc.cl>
2021-08-20 15:36:15 -0400

    :sparkles: Add base pages

--- SimplyRecipes/src/pages/about.tsx
+++ SimplyRecipes/src/pages/about.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { PageProps } from 'gatsby';
 
-const IndexPage: React.FC<PageProps> = () => {
+const AboutPage: React.FC<PageProps> = () => {
   return <div>About page</div>;
 };
 
-export default IndexPage;
+export default AboutPage;
---
+++ SimplyRecipes/src/pages/contact.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PageProps } from 'gatsby';
+
+const ContactPage: React.FC<PageProps> = () => {
+  return <div>Contact page</div>;
+};
+
+export default ContactPage;
