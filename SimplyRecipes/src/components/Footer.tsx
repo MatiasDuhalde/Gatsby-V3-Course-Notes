9deb70c414e23123ae7ff6b7208310e37b9aec6c
Matias Duhalde <matias.duhalde@uc.cl>
2021-08-22 00:39:05 -0400

    :sparkles: Add TS type

--- SimplyRecipes/src/components/Footer.tsx
+++ SimplyRecipes/src/components/Footer.tsx
@@ -1,7 +1,7 @@
-import { makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
+import { makeStyles, Theme, Typography } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => {
+const useStyles = makeStyles((theme: Theme) => {
   return {
     root: {
       display: 'flex',
