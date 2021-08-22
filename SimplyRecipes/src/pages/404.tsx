3a5e2e4737925ff9562106a642f682c49fa12003
Matias Duhalde <matias.duhalde@uc.cl>
2021-08-22 00:39:32 -0400

    :sparkles: Better 404 page

--- SimplyRecipes/src/pages/404.tsx
+++ SimplyRecipes/src/pages/404.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import { PageProps } from 'gatsby';
+import { makeStyles, Theme, Typography } from '@material-ui/core';
 import Layout from '../components/Layout';
 
-const Error404Page: React.FC<PageProps> = () => (
-  <Layout>
-    <h1>You are here!</h1>
-    <h2>But nothing found for you #404</h2>
-  </Layout>
-);
+const useStyles = makeStyles((theme: Theme) => {
+  return {
+    root: {},
+    errorDisplay: {
+      textAlign: 'center',
+      paddingTop: theme.spacing(5),
+      paddingBottom: theme.spacing(5),
+      '& h1': {
+        fontSize: `calc(${theme.typography.h1.fontSize} * 1.5)`,
+      },
+    },
+  };
+});
+
+const Error404Page: React.FC<PageProps> = () => {
+  const classes = useStyles();
+  return (
+    <Layout>
+      <main className={classes.root}>
+        <section className={classes.errorDisplay}>
+          <Typography variant="h1" color="textPrimary" gutterBottom>
+            404
+          </Typography>
+          <Typography variant="h3" color="textPrimary" gutterBottom>
+            Page Not Found
+          </Typography>
+        </section>
+      </main>
+    </Layout>
+  );
+};
 
 export default Error404Page;
