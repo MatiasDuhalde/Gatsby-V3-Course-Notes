c7d4f6f8924c576f5da8b54e68abd898f469249a
Matias Duhalde <matias.duhalde@uc.cl>
2021-08-22 03:49:49 -0400

    :sparkles: Add custom typescript types

---
+++ SimplyRecipes/src/custom.d.ts
@@ -0,0 +1,8 @@
+declare module '*.png' {
+  const content: React.FunctionComponent<React.ImgHTMLAttributes<HTMLImageElement>>;
+  export default content;
+}
+declare module '*.svg' {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
