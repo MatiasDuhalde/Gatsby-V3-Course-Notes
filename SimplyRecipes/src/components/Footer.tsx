15811fc6c35037cc66a6cf286b12666321ec4686
Matias Duhalde <matias.duhalde@uc.cl>
2021-08-21 20:39:19 -0400

    :sparkles: Configure theme and footer

--- SimplyRecipes/src/components/Footer.tsx
+++ SimplyRecipes/src/components/Footer.tsx
@@ -1,9 +1,33 @@
+import { makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles(theme => {
+  return {
+    root: {
+      display: 'flex',
+      height: '4rem',
+      textAlign: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: theme.palette.grey[900],
+      padding: theme.spacing(3),
+      color: theme.palette.grey[200],
+      '& h5': { marginBottom: 0 },
+      '& p': { marginBottom: 0 },
+      '& span, & a': { color: theme.palette.primary.light, textDecoration: 'none' },
+    },
+  };
+});
+
 const Footer: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <footer>
-      <h3>Footer Here</h3>
+    <footer className={classes.root}>
+      <Typography variant="body2">
+        &copy; {new Date().getFullYear()} <span>SimplyRecipes</span>. Built with{' '}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </Typography>
     </footer>
   );
 };
--- SimplyRecipes/src/components/Layout.tsx
+++ SimplyRecipes/src/components/Layout.tsx
@@ -3,10 +3,12 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import Footer from './Footer';
 import Navbar from './Navbar';
 import theme from '../theme';
+import { CssBaseline } from '@material-ui/core';
 
 const Layout: React.FC = props => {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Navbar />
       {props.children}
       <Footer />
