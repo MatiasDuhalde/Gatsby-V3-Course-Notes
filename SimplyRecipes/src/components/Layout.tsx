9fbd89eff644a63b732481bb467f9311602c1a02
Matias Duhalde <matias.duhalde@uc.cl>
2021-08-28 16:51:50 -0400

    :recycle: Remove redundant styles in Layout component

--- SimplyRecipes/src/components/Layout.tsx
+++ SimplyRecipes/src/components/Layout.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
-import { CssBaseline, makeStyles } from '@material-ui/core';
+import { CssBaseline } from '@material-ui/core';
 import Footer from './Footer';
 import Navbar from './Navbar';
 import theme from '../theme';
 
-const useStyles = makeStyles(() => {
-  return {
-    root: {
-      a: {
-        textDecoration: 'none',
-      },
-    },
-  };
-});
-
 const Layout: React.FC = props => {
-  const classes = useStyles();
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline classes={classes.root} />
-      <Navbar />
-      {props.children}
-      <Footer />
+      <>
+        <CssBaseline />
+        <Navbar />
+        {props.children}
+        <Footer />
+      </>
     </ThemeProvider>
   );
 };
