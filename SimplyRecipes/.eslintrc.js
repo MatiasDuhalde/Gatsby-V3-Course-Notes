b16380caf9b4d6b5f40c71e931e9251f7cd0fe85
Matias Duhalde <matias.duhalde@uc.cl>
2021-08-27 21:51:41 -0400

    :rotating_light: Update eslint rules

--- SimplyRecipes/.eslintrc.js
+++ SimplyRecipes/.eslintrc.js
@@ -1,37 +1,65 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
-  },
   env: {
     browser: true,
     node: true,
+    es6: true,
   },
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 'next',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['import', 'react', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   rules: {
-    quotes: 'off',
-    '@typescript-eslint/quotes': [
-      2,
-      'single',
-      {
-        avoidEscape: true,
-      },
-    ],
-    'prettier/prettier': [
+    '@typescript-eslint/naming-convention': [
       'error',
       {
-        trailingComma: 'es5',
-        semi: true,
-        singleQuote: true,
-        printWidth: 100,
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true,
+        },
       },
     ],
+    'react/prop-types': 'off',
   },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/indent': 'off',
+      },
+    },
+  ],
 };
