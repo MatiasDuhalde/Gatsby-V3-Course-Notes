35db6411e280a63f2c6f46100256564aa7ff6f97
Matias Duhalde <matias.duhalde@uc.cl>
2021-08-27 21:52:34 -0400

    :wrench: Add contentful plugin

--- SimplyRecipes/gatsby-config.js
+++ SimplyRecipes/gatsby-config.js
@@ -3,9 +3,18 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Simply Recipes',
+    description: 'Nice and clean recipes site',
+    author: '@MatiasDuhalde',
+    siteUrl: 'https://simplyrecipes.com',
+  },
   plugins: [
     {
       resolve: 'gatsby-plugin-layout',
@@ -17,5 +26,19 @@ module.exports = {
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/assets/images/`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ],
 };
